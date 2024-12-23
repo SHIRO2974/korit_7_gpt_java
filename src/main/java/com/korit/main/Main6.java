ab3a54c1a13856acb3e5da0c3c8ebfd4160b779a
SHIRO2974 <wogus2974@naver.com>
2024-12-23 12:03:18 +0900

    Predicate

--- src/main/java/com/korit/main/Main6.java
+++ src/main/java/com/korit/main/Main6.java
@@ -1,19 +1,21 @@
 package com.korit.main;
 
-import java.util.Iterator;
+import java.util.*;
 import java.util.function.*;
+import java.util.stream.Collectors;
 
 public class Main6 {
     public static void main(String[] args) {
-        Runnable runnable;
-        Consumer<String> consumer;
-        Supplier<String> supplier;
-        Function<String, String> function;
+
+//      조건을 체크하는 역할 입력값을 받아 true,false 를 반환
         Predicate<String> predicate;
-        BiConsumer<Integer, String> biConsumer;
-        BiFunction<Integer, Integer, String> biFunction;
+
+        List<Integer> nums = List.of(1,2,3,4,5);
+        List<Integer> evenNums = nums.stream()
+                .filter(num -> num % 2 == 0)    // 2로 나누었을때 0으로 떨어지면 true, 그렇지 않으면 false
+                .collect(Collectors.toList());
+
         BiPredicate<Integer, Integer> biPredicate;
-        UnaryOperator<Integer> unaryOperator;
-        BinaryOperator<Double> binaryOperator;
+
     }
 }
